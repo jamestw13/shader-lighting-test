aa3d1e5850d87cd01eb0e668d922f41c1767dd25
TJ James <jamestw13@gmail.com>
2024-04-11 22:43:27 -0500

    Created ambient and directional light functions for shaders

---
+++ src/shaders/includes/directionalLight.glsl
@@ -0,0 +1,9 @@
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition) {
+  
+  vec3 lightDirection = normalize(lightPosition);
+
+  // Shading
+  float shading = dot(normal, lightDirection);
+  return vec3(shading);
+  // return lightColor * lightIntensity;
+}
--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,8 +1,27 @@
 uniform vec3 uColor;
 
+varying vec3 vNormal;
+
+#include ../includes/ambientLight.glsl;
+#include ../includes/directionalLight.glsl;
+
 void main()
 {
     vec3 color = uColor;
+
+    // Light
+    vec3 light = vec3(0.0);
+    // light += ambientLight(
+    //     vec3(1.0),          // light color
+    //     0.03                // light intensity
+    //     );
+    light += directionalLight(
+        vec3(0.1, 0.1, 1.0),   // light color
+        1.0,                   // light intensity
+        vNormal,               // Normal
+        vec3(0.0, 0.0, 3.0)    // Light position
+        );
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
